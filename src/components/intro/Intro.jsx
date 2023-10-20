4f49df092dd227b522e9c78fc516d5d13f7ac5ce
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-20 18:39:39 +0000

    feat: Add and style an Introductory Card component

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,14 @@
+import "./Intro.css";
+
+function Intro() {
+    return (
+        <div className="intro-card">
+            <p>Hi, This is me - Philip!</p>
+            <p>Right now, I am 23 years  old and I live in Accra</p>
+            <p>My full name is Philip Obiri and I am an experienced web and mobile app developer </p>
+            <p>I take pride in crafting user-friendly interfaces </p>
+        </div>
+    )
+}
+
+export default Intro;
