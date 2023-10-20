685b539c290ca4bd0e75583f5113cc1c1d01cd0b
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-20 18:33:39 +0000

    feat: Main App

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './App.css';
+
+function SearchBar(props) {
+  return <input type="search" onChange={props.onUpdateSearch} />;
+};
+
+
+function Overview({ currentTerm }) {
+  return <p>Currently searching for {currentTerm ? `"${currentTerm}"` : "..."}</p>;
+};
+
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  
+  function updateSearchTermHandler(event) {
+    setSearchTerm(event.target.value);
+  };
+
+
+  return (
+    <div className='container'>
+      <SearchBar onUpdateSearch={updateSearchTermHandler} />
+      <Overview currentTerm={searchTerm} />
+    </div>
+  );
+};
+
+export default App;
